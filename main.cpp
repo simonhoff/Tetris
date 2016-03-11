ea2180ed54c90cec7f7637113ef45096d7114ad3
simonhoff <simonaho@stud.ntnu.no>
2016-03-11 12:35:30 +0100

    added main

--- main.cpp
+++ main.cpp
@@ -1,1 +1,9 @@
+#include <SFML/Graphics.hpp>
 
+int main(){
+	int rows = 20;
+	int cols = 10;
+	const int WIDTH = 40*rows + 4*40 + 10*40;
+	const int HEIGHT = 40*cols + 3*40;
+
+	sf::RenderWindow window(sf::Videomode(WIDTH,HEIGHT), "Tetris");
